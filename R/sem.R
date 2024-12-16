bd82878e41ce9715362945a082b4edc59b4142f8
Julius Pfadt <julius.pfadt@gmail.com>
2024-12-16 17:11:53 +0100

    fix encoded names display in total effects table

--- R/sem.R
+++ R/sem.R
@@ -20,6 +20,9 @@
 SEMInternal <- function(jaspResults, dataset, options, ...) {
   jaspResults$addCitation("Rosseel, Y. (2012). lavaan: An R Package for Structural Equation Modeling. Journal of Statistical Software, 48(2), 1-36. URL http://www.jstatsoft.org/v48/i02/")
 
+  # sink(file="~/Downloads/log.txt")
+  # on.exit(sink(NULL))
+
   # Read dataset
   options <- .semPrepOpts(options)
 
@@ -624,10 +627,10 @@ checkLavaanModel <- function(model, availableVars) {
     }
 
     total_effects <- c()
-    for (pred in unique(c(pred,regressions[[group]][["rhs"]]))) {
+    for (pred in unique(c(pred, regressions[[group]][["rhs"]]))) {
       for (out in unique(c(out, regressions[[group]][["lhs"]]))) {
         total_effect <- NULL
-        total_effect_name <- paste0("total_", groups[group], "_", pred, "_", out)
+        total_effect_name <- paste0("total_", groups[group], "__", pred, "__", out)
         indirect_effect_idx <- unlist(lapply(names(indirect_effects), function(x) {
           startsWith(x, pred) && endsWith(x, out)
         }))
@@ -1195,10 +1198,6 @@ checkLavaanModel <- function(model, availableVars) {
 
   }
 
-  # TODO: bootstrap standardized CI:
-  #' Do we want the standardization to happen for the fit of each bootstrap sample and then save the std coeffs?
-  #' Or do we want to save all the unstd coeffs from the bootstrap, and after summarizing them with mean
-  #' and se and such standardize them?
 
   if (options[["group"]] != "")  {
     pe[pe[["op"]] != ":=", "groupname"] <- lavaan::lavInspect(fit, "group.label")[pe[["group"]]]
@@ -1382,8 +1381,22 @@ checkLavaanModel <- function(model, availableVars) {
 
     path <- list()
     for (idx in 1:nrow(pe_toteff)) {
-      path[[idx]] <- gsub("_", " \u2192 ", pe_toteff[idx, "lhs"])
-      path[[idx]] <- gsub("total \u2192", "", path[[idx]])
+      # path[[idx]] <- gsub("_", " \u2192 ", pe_toteff[idx, "lhs"])
+      # path[[idx]] <- gsub("total \u2192", "", path[[idx]])
+
+      # Step 1: Remove the "total_X__" prefix (X can be a number or word)
+      path[[idx]] <- gsub("^total_[^_]+__", "", pe_toteff[idx, "lhs"])
+
+      # Step 2: Decode any "JaspColumn_X_Encoded" variable names
+      matches <- regmatches(path[[idx]], gregexpr("JaspColumn_[0-9]+_Encoded", path[[idx]]))
+      if (length(matches[[1]]) > 0) {
+        decoded_matches <- sapply(matches[[1]], jaspBase::decodeColNames) # Decode each match
+        regmatches(path[[idx]], gregexpr("JaspColumn_[0-9]+_Encoded", path[[idx]])) <- list(decoded_matches)
+      }
+
+      # Step 3: Replace double underscores "__" with " → "
+      path[[idx]] <- gsub("__", " \u2192 ", path[[idx]])
+
       for (group in groups)
         path[[idx]] <- gsub(paste0(" ", group, " \u2192"), "", path[[idx]])
 
