de08b183f3b381be2e7100cbf82213e8b5c91848
Julius Pfadt <julius.pfadt@gmail.com>
2025-03-25 13:25:47 +0100

    move towards preloadData

--- R/sem.R
+++ R/sem.R
@@ -20,6 +20,10 @@
 SEMInternal <- function(jaspResults, dataset, options, ...) {
   jaspResults$addCitation("Rosseel, Y. (2012). lavaan: An R Package for Structural Equation Modeling. Journal of Statistical Software, 48(2), 1-36. URL http://www.jstatsoft.org/v48/i02/")
 
+  saveRDS(options, "~/Downloads/options.rds")
+
+  saveRDS(dataset, "~/Downloads/dataset.rds")
+
   # Read dataset
   options <- .semPrepOpts(options)
 
