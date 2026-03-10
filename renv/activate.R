bf7955fb5a81e788456a40b6d90c2f51219a99a4
Julius Pfadt <julius.pfadt@gmail.com>
2026-03-10 17:10:45 +0100

    update renv.lock and tests

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.1.7"
-  attr(version, "md5") <- "dd5d60f155dadff4c88c2fc6680504b4"
+  version <- "1.1.8"
+  attr(version, "md5") <- "be71f6cdf4c947eebbffbf9fd2489319"
   attr(version, "sha") <- NULL
 
   # the project directory
