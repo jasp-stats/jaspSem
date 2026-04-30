965c0b3a735757af00da41eeb36d48756a0ed91f
Julius Pfadt <julius.pfadt@gmail.com>
2026-04-30 13:23:07 +0200

    revert stan lock file stuff

--- tests/testthat/test-bayesiansem.R
+++ tests/testthat/test-bayesiansem.R
@@ -91,7 +91,7 @@ test_that("Factor Loadings traceplot exists", {
 
 test_that("Fit Indices table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_addfit"]][["collection"]][["modelContainer_addfit_fitTable_1"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.0977812449233765, "Bayesian RMSEA", 0.0441573130088842, 0.0980718607777259,
                                       0.0274811509445776, 0.151686965781034, 0.958556607896851, "Bayesian Gamma Hat",
                                       0.915347972685177, 0.960826387426461, 0.020590958806948, 0.993297051069999,
@@ -108,14 +108,14 @@ test_that("Fit Indices table results match", {
 
 test_that("Model Fit table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_fittab"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1906.36346173569, 1905.83340954543, "Model 1", 75, 0.1575, 1905.62733904596,
                                       15, 15))
 })
 
 test_that("Factor Loadings table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_ind"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1, 1, 1, "", "ind60", "", "", "", "x1", 0, 1.9532168575293, 2.56517267860099,
                                       2.23915232240683, "", "ind60", 234.158080082042, "normal(0,10)",
                                       0.997693090997038, "x2", 0.160873314749524, 1.5402366210022,
@@ -132,7 +132,7 @@ test_that("Factor Loadings table results match", {
 
 test_that("Factor Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_lvar"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.283932707489185, 0.664706603654407, 0.446681847345986, "", "ind60",
                                       332.538342458098, "gamma(1,0.5)[sd]", 1.00191626886132, 0.0950770068678501,
                                       1.71418079014573, 5.56841007837074, 3.41220758029049, "", "dem60",
@@ -142,7 +142,7 @@ test_that("Factor Variances table results match", {
 
 test_that("Regression Coefficients table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_reg"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.608355320401092, 2.08918040399425, 1.35613928209665, "", "dem60",
                                       112.582981008041, "normal(0,10)", 1.02054836443106, "ind60",
                                       0.362601909681302))
@@ -150,7 +150,7 @@ test_that("Regression Coefficients table results match", {
 
 test_that("Residual Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_var"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.0537626650941761, 0.139315006891883, 0.0913901143044193, "",
                                       "x1", 173.961694715611, "gamma(1,0.5)[sd]", 0.999309053656452,
                                       0.022396773153692, 8.049983539546e-05, 0.277406757498406, 0.104709707889598,
@@ -194,14 +194,14 @@ results <- jaspTools::runAnalysis("BayesianSEM", testthat::test_path("poldem_gro
 
 test_that("Model Fit table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_fittab"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1881.32522055122, 1882.20127389458, "Model 1", 75, 1882.03059237359,
                                       26, 30))
 })
 
 test_that("Factor Loadings table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_ind"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1, 1, 1, 1, "", "ind60", "", "x1", 0, 2.01007229197278, 2.66440161565327,
                                       2.28762428711893, 1, "", "ind60", "normal(0,10)", "x2", 0.15858242401967,
                                       1.57369817320054, 2.24376688416746, 1.89090831722004, 1, ".p3.",
@@ -226,7 +226,7 @@ test_that("Factor Loadings table results match", {
 
 test_that("Factor Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_lvar"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.263226572626372, 0.732043056317347, 0.446421435173051, 1, "",
                                       "ind60", "gamma(1,0.5)[sd]", 0.124955136019569, 1.28779551158517,
                                       5.19334305313828, 2.83816884164325, 1, "", "dem60", "gamma(1,0.5)[sd]",
@@ -238,7 +238,7 @@ test_that("Factor Variances table results match", {
 
 test_that("Regression Coefficients table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_reg"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.815953562643048, 2.60118593824993, 1.67581745535033, 1, "",
                                       "dem60", "normal(0,10)", "ind60", 0.441498861667027, -0.312456087676206,
                                       2.01243645531556, 0.907505231073783, 2, "", "dem60", "normal(0,10)",
@@ -247,7 +247,7 @@ test_that("Regression Coefficients table results match", {
 
 test_that("Residual Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_var"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.0300708380067292, 0.101501895696027, 0.0613096065596296, 1,
                                       "", "x1", "gamma(1,0.5)[sd]", 0.0183001853668487, 5.42817560149972e-06,
                                       0.17569704600619, 0.0396057404498012, 1, "", "x2", "gamma(1,0.5)[sd]",
@@ -342,7 +342,7 @@ test_that("Model Fit table adds LOO comparison for multiple models", {
 
 test_that("Model Fit table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_fittab"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1906.36346173569, 1905.83340954543, "Model 1", 75, 1, 1905.62733904596,
                                       0, 15, 15, 0, 1908.00800997856, 1907.12863445693, "Model 2",
                                       75, 2, 1907.00252475413, -0.642894015831216, 15, 15, 0.485106016309889
@@ -351,7 +351,7 @@ test_that("Model Fit table results match", {
 
 test_that("Factor Loadings table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 1"]][["collection"]][["modelContainer_params_Model 1_ind"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1, 1, 1, "", "ind60", "", "x1", 0, 1.9532168575293, 2.56517267860099,
                                       2.23915232240683, "", "ind60", "normal(0,10)", "x2", 0.160873314749524,
                                       1.5402366210022, 2.16807785385677, 1.84369511512563, "", "ind60",
@@ -366,7 +366,7 @@ test_that("Factor Loadings table results match", {
 
 test_that("Factor Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 1"]][["collection"]][["modelContainer_params_Model 1_lvar"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.283932707489185, 0.664706603654407, 0.446681847345986, "", "ind60",
                                       "gamma(1,0.5)[sd]", 0.0950770068678501, 1.71418079014573, 5.56841007837074,
                                       3.41220758029049, "", "dem60", "gamma(1,0.5)[sd]", 1.02470540118021
@@ -375,14 +375,14 @@ test_that("Factor Variances table results match", {
 
 test_that("Regression Coefficients table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 1"]][["collection"]][["modelContainer_params_Model 1_reg"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.608355320401092, 2.08918040399425, 1.35613928209665, "", "dem60",
                                       "normal(0,10)", "ind60", 0.362601909681302))
 })
 
 test_that("Residual Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 1"]][["collection"]][["modelContainer_params_Model 1_var"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.0537626650941761, 0.139315006891883, 0.0913901143044193, "",
                                       "x1", "gamma(1,0.5)[sd]", 0.022396773153692, 8.049983539546e-05,
                                       0.277406757498406, 0.104709707889598, "", "x2", "gamma(1,0.5)[sd]",
@@ -398,7 +398,7 @@ test_that("Residual Variances table results match", {
 
 test_that("Factor Loadings table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 2"]][["collection"]][["modelContainer_params_Model 2_ind"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(1, 1, 1, "", "ind60", "", "x1", 0, 1.96519068564153, 2.51673059616912,
                                       2.22745627318565, "", "ind60", "normal(0,10)", "x2", 0.147109264536351,
                                       1.52392573381841, 2.14033985258738, 1.83664770518682, "", "ind60",
@@ -413,14 +413,14 @@ test_that("Factor Loadings table results match", {
 
 test_that("Factor Covariances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 2"]][["collection"]][["modelContainer_params_Model 2_lcov"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.278349696338848, 1.04975295035336, 0.598487745834573, "", "ind60 - dem60",
                                       "lkj_corr(1)", 0.199283825243998))
 })
 
 test_that("Factor Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 2"]][["collection"]][["modelContainer_params_Model 2_lvar"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.303144015762355, 0.703077589685985, 0.462209301645044, "", "ind60",
                                       "gamma(1,0.5)[sd]", 0.103785215027818, 2.13184089890503, 6.65951675159269,
                                       3.95327378859623, "", "dem60", "gamma(1,0.5)[sd]", 1.15069819421129
@@ -429,7 +429,7 @@ test_that("Factor Variances table results match", {
 
 test_that("Residual Variances table results match", {
   table <- results[["results"]][["modelContainer"]][["collection"]][["modelContainer_params"]][["collection"]][["modelContainer_params_Model 2"]][["collection"]][["modelContainer_params_Model 2_var"]][["data"]]
-  jaspTools::expect_equal_tables(table,
+  expect_bsem_table(table,
                                  list(0.0494395895825296, 0.135638795976496, 0.0907033045381739, "",
                                       "x1", "gamma(1,0.5)[sd]", 0.0224170838077918, 0.00907833252556717,
                                       0.289710986087315, 0.112435158682542, "", "x2", "gamma(1,0.5)[sd]",
